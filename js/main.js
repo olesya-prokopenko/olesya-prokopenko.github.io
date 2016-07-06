c3b642724e27866c8d627a47761190351cc22bd1
Olesya Prokopenko <oles.prokopenko@gmail.com>
2016-07-06 20:20:55 +0600

    add tweenmax

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// window.onload = function(){
+//   var text = document.getElementById("test");
+//   TweenLite.to(text, 1, {left:"632px"});
+// }
+
+function load() {
+  console.log("load event detected!");
+}
+window.onload = load;
