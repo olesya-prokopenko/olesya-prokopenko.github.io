021c8c306059e0743a973eac5db8c10a0187d314
Olesya Prokopenko <oles.prokopenko@gmail.com>
2017-10-10 18:08:26 +0700

    redisign my-page

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+/* global require */
+
 // Include gulp
 var gulp = require('gulp');
 
@@ -5,20 +7,36 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 
 var postcss = require('gulp-postcss'),
-    autoprefixer = require('autoprefixer'),
-    lost = require('lost');
+  autoprefixer = require('autoprefixer');
 
 gulp.task('css', function () {
+  "use strict";
+
   gulp
-      .src('./less/main.less')
-      .pipe(less())
-      .pipe(postcss([
-        lost(),
-        autoprefixer()
-      ]))
-      .pipe(gulp.dest('./css'));
+    .src('./less/**/*.less')
+    .pipe(less({
+      strictMath: true
+    }))
+    .pipe(postcss([
+      autoprefixer({
+        browsers: [
+          '> 1%',
+          'last 3 versions',
+          'iOS >= 7',
+          'IE >= 9',
+          'Firefox >= 18',
+          'Opera >= 14',
+          'Safari >= 8',
+          'Android >= 2.1',
+          'ExplorerMobile >= 10'
+        ]
+      })
+    ]))
+    .pipe(gulp.dest('./css'));
 });
 
 gulp.task('watch', function () {
+  "use strict";
+
   gulp.watch('./less/**/*.less', ['css']);
 });
--- js/main.js
+++
@@ -1,9 +0,0 @@
-// window.onload = function(){
-//   var text = document.getElementById("test");
-//   TweenLite.to(text, 1, {left:"632px"});
-// }
-
-function load() {
-  console.log("load event detected!");
-}
-window.onload = load;
